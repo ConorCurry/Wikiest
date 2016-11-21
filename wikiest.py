7654aee0b10d115ee83661198efceb18598317ab
ConorCurry <conorcurry@gmail.com>
2016-11-21 11:17:59 +0100

    Added top listing for prefixed words
    It's not working correctly though. The list gets too long and doesn't
    update appropriately. Adding unit tests on a new branch.

--- wikiest.py
+++ wikiest.py
@@ -7,9 +7,23 @@ def Tree():
 def addWord(tree, word, score):
 	#_traverse(tree, word, add=True)['score'] = score
 	cur = tree
-	for let in word.lower():
+	for lev, let in enumerate(word.lower()):
 		cur = cur.__getitem__(let)
+		if 'top' not in cur: cur['top'] = []
+		_computeTop(cur['top'], (score,word[lev+1:]))
+	cur['score'] = score
 	return cur
+
+def _computeTop(top, scorePair, topSize=3):
+	for i in range(topSize):
+		if i >= len(top) - 1:
+			top.append(scorePair)
+			return True
+		if scorePair[0] > top[i][0]:
+			top.insert(i, scorePair)
+			if(len(top)) > 3: top.pop()
+			return True
+	return False
 
 def getWord(tree, word):
 	#return _traverse(tree, word)
@@ -17,15 +31,15 @@ def getWord(tree, word):
 	for let in word.lower():
 		if let not in cur: return False
 		cur = cur.__getitem__(let)
-	return cur
+	return cur['top'], cur
 
-def _traverse(tree, word, add=False):
-	cur = tree
-	for let in word.lower():
-		if (not add) and let not in cur:
-			return False
-		cur = cur.__getitem__(let)
-	return cur
+# def _traverse(tree, word, add=False):
+# 	cur = tree
+# 	for let in word.lower():
+# 		if (not add) and let not in cur:
+# 			return False
+# 		cur = cur.__getitem__(let)
+# 	return cur
 
 # class Node(letter, score):
 # 	self.letter = letter
